3e6121acf91fa6d24b600611ef9eb78d5d3879b1
conarti <abelous2009@gmail.com>
2022-12-31 13:07:20 +0300

    project init, add 'path-checker rule'

---
+++ README.md
@@ -0,0 +1,48 @@
+# eslint-plugin-fsd-react
+
+Feature-sliced design methodology plugin for react
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-fsd-react`:
+
+```sh
+npm install eslint-plugin-fsd-react --save-dev
+```
+
+## Usage
+
+Add `fsd-react` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "fsd-react"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "fsd-react/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/path-checker.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Checks for absolute and relative paths
+ * @author conarti
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/path-checker"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("path-checker", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
+});
