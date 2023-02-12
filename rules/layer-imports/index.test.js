fc66b6998762a82e9ba103c0fa330b757ac2b676
conarti <abelous2009@gmail.com>
2023-02-12 11:34:49 +0300

    test(layer-imports): setup tests for new implementation without 'alias' option

--- rules/layer-imports/index.test.js
+++ rules/layer-imports/index.test.js
@@ -2,12 +2,6 @@ const { RuleTester } = require('eslint');
 const rule = require('./index');
 const { errorCodes } = require('../../lib/constants');
 
-const aliasOptions = [
-  {
-    alias: '@',
-  },
-];
-
 const ruleTester = new RuleTester({
   parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 });
@@ -18,37 +12,31 @@ ruleTester.run('layer-imports', rule, {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\features\\Article',
       code: 'import { addCommentFormActions, addCommentFormReducer } from \'@/shared/Button.tsx\'',
       errors: [],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\features\\Article',
       code: 'import { addCommentFormActions, addCommentFormReducer } from \'@/entities/Article\'',
       errors: [],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\app\\providers',
       code: 'import { addCommentFormActions, addCommentFormReducer } from \'@/widgets/Articl\'',
       errors: [],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\widgets\\pages',
       code: 'import { useLocation } from \'react-router-dom\'',
       errors: [],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\app\\providers',
       code: 'import { addCommentFormActions, addCommentFormReducer } from \'redux\'',
       errors: [],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\index.tsx',
       code: 'import { StoreProvider } from \'@/app/providers/StoreProvider\';',
       errors: [],
-      options: aliasOptions,
     },
   ],
 
@@ -61,7 +49,6 @@ ruleTester.run('layer-imports', rule, {
           messageId: errorCodes['layer-imports'],
         },
       ],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\features\\providers',
@@ -71,7 +58,6 @@ ruleTester.run('layer-imports', rule, {
           messageId: errorCodes['layer-imports'],
         },
       ],
-      options: aliasOptions,
     },
     {
       filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\providers',
@@ -81,7 +67,6 @@ ruleTester.run('layer-imports', rule, {
           messageId: errorCodes['layer-imports'],
         },
       ],
-      options: aliasOptions,
     },
   ],
 });
