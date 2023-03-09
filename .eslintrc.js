90b22b61e7024d7f9d51a05a5fafca53fecd7136
conarti <abelous2009@gmail.com>
2023-03-09 10:28:21 +0300

    chore(eslint): update config rules
    Add filename validation, unused imports and other stylistic rules. Also fix the rules setting for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line filenames-simple/naming-convention
 "use strict";
 
 module.exports = {
@@ -7,13 +8,49 @@ module.exports = {
     "plugin:eslint-plugin/recommended",
     "plugin:node/recommended",
   ],
+  plugins: [
+    'filenames-simple',
+    'unused-imports',
+  ],
   env: {
     node: true,
   },
+  rules: {
+    'filenames-simple/naming-convention': ['error', { rule: 'kebab-case' }],
+    'unused-imports/no-unused-imports': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'indent': ['error', 2],
+    'brace-style': 'error',
+    'semi': ['error', 'always'],
+    'object-property-newline': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'object-curly-newline': ['error', {
+      'ObjectExpression': {
+        multiline: true,
+        minProperties: 2,
+        consistent: true,
+      },
+      'ObjectPattern': {
+        multiline: true,
+        minProperties: 2,
+      },
+      'ImportDeclaration': {
+        multiline: true,
+        consistent: true,
+      },
+      'ExportDeclaration': {
+        multiline: true,
+        minProperties: 2,
+        consistent: true,
+      },
+    }],
+  },
   overrides: [
     {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
+      files: ["**/*.test.js"],
+      env: {
+        mocha: true,
+      },
     },
   ],
 };
