71bcfcde6f36d99041fe1b83e27c4ee8191bb5eb
conarti <abelous2009@gmail.com>
2023-05-25 22:47:57 +0300

    refactor(layers-slices): remove 'isValidByRuleOptions' from 'validate' function

--- rules/layers-slices/model/validate-and-report.js
+++ rules/layers-slices/model/validate-and-report.js
@@ -1,11 +1,16 @@
 const { ERROR_MESSAGE_ID } = require('../constants');
 const { validate } = require('./validate');
 const { extractPathsInfo } = require('./extract-paths-info');
+const { isValidByRuleOptions } = require('./is-valid-by-rule-options');
 
 module.exports.validateAndReport = function(node, context, ruleOptions) {
   const pathsInfo = extractPathsInfo(node, context);
 
-  if (validate(pathsInfo, ruleOptions)) {
+  if (isValidByRuleOptions(pathsInfo, ruleOptions)) {
+    return;
+  }
+
+  if (validate(pathsInfo)) {
     return;
   }
 
--- rules/layers-slices/model/validate.js
+++ rules/layers-slices/model/validate.js
@@ -1,6 +1,5 @@
 const { canImportLayer } = require('./can-import-layer');
-const { isValidByRuleOptions } = require('./is-valid-by-rule-options');
 
-module.exports.validate = function(pathsInfo, ruleOptions){
-  return isValidByRuleOptions(pathsInfo, ruleOptions) || canImportLayer(pathsInfo);
+module.exports.validate = function(pathsInfo){
+  return canImportLayer(pathsInfo);
 };
