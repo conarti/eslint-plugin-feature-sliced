fb2163a86fd176f2997f21d24297fa3d6b9fd746
conarti <abelous2009@gmail.com>
2023-05-26 09:49:46 +0300

    refactor(layers-slices): simplify 'can-import-layer' logic

--- rules/layers-slices/model/can-import-layer.js
+++ rules/layers-slices/model/can-import-layer.js
@@ -8,31 +8,20 @@ module.exports.canImportLayer = (pathsInfo) => {
     currentFileSlice,
   } = pathsInfo;
 
-  if (!currentFileSlice) {
-    return true;
-  }
-
   const isImportFromSameSlice = importSlice === currentFileSlice;
 
-  if (isImportFromSameSlice) {
-    return true;
-  }
-
   const hasUnknownLayers = !layersMap.has(importLayer) || !layersMap.has(currentFileLayer);
-
-  if (hasUnknownLayers) {
-    return true;
-  }
 
   const isInsideShared = importLayer === 'shared' && currentFileLayer === 'shared';
   const isInsideApp = importLayer === 'app' && currentFileLayer === 'app';
 
-  if (isInsideShared || isInsideApp) {
-    return true;
-  }
-
   const importLayerOrder = layersMap.get(importLayer);
   const currentFileLayerOrder = layersMap.get(currentFileLayer);
 
-  return currentFileLayerOrder > importLayerOrder;
+  return !currentFileSlice
+    || isImportFromSameSlice
+    || hasUnknownLayers
+    || isInsideShared
+    || isInsideApp
+    || currentFileLayerOrder > importLayerOrder;
 };
