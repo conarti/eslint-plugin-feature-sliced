31ce862775ff548f3f72524cd45e7524ed5adda0
conarti <abelous2009@gmail.com>
2023-05-28 00:42:18 +0300

    style(public-api): constants export style

--- rules/public-api/constants.js
+++ rules/public-api/constants.js
@@ -1,9 +1,14 @@
-module.exports.MESSAGE_ID = {
+const MESSAGE_ID = {
   SHOULD_BE_FROM_PUBLIC_API: 'should-be-from-public-api',
   REMOVE_SUGGESTION: 'remove-suggestion',
 };
 
-module.exports.IGNORED_LAYERS = new Set([
+const IGNORED_LAYERS = new Set([
   'app',
   'shared',
 ]);
+
+module.exports = {
+  MESSAGE_ID,
+  IGNORED_LAYERS,
+};
