c6d264aa9f0226162842e758af04082ca418edc1
conarti <abelous2009@gmail.com>
2023-05-27 11:52:52 +0300

    feat(absolute-relative): improve logic for getting layer and slice from import path
    Now works with relative paths too

--- rules/absolute-relative/model/extract-paths-info.js
+++ rules/absolute-relative/model/extract-paths-info.js
@@ -1,6 +1,7 @@
 const {
   normalizePath,
   getLayerSliceFromPath,
+  convertToAbsolute,
 } = require('../../../lib/helpers');
 
 /* @duplicate - the same function at 'layers-slices' */
@@ -11,7 +12,9 @@ module.exports.extractPathsInfo = function (node, context) {
   const normalizedCurrentFilePath = normalizePath(currentFilePath);
   const normalizedImportPath = normalizePath(importPath);
 
-  const [importLayer, importSlice] = getLayerSliceFromPath(normalizedImportPath);
+  const importAbsolutePath = convertToAbsolute(normalizedCurrentFilePath, normalizedImportPath);
+
+  const [importLayer, importSlice] = getLayerSliceFromPath(importAbsolutePath);
   const [currentFileLayer, currentFileSlice] = getLayerSliceFromPath(normalizedCurrentFilePath);
 
   return {
