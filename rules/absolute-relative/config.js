15d8b4771ca0410d910607ccddbfdc0967664157
conarti <abelous2009@gmail.com>
2023-05-28 16:01:07 +0300

    style(absolute-relative): update reexport style

--- rules/absolute-relative/config.js
+++ rules/absolute-relative/config.js
@@ -1,4 +1,8 @@
-module.exports.ERROR_MESSAGE_ID = {
+const ERROR_MESSAGE_ID = {
   MUST_BE_RELATIVE_PATH: 'must-be-relative-path',
   MUST_BE_ABSOLUTE_PATH: 'must-be-absolute-path',
+};
+
+module.exports = {
+  ERROR_MESSAGE_ID,
 };
