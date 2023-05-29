21f4af4d40ec04e1db9a5be40939b7ebc514c04d
conarti <abelous2009@gmail.com>
2023-05-29 13:25:03 +0300

    docs(site): enable local search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    ],
+
+    search: {
+      provider: 'local'
+    }
   }
 })
