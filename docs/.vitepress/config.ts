296c4d3237f51891ae8390bdce3f889d59184d2e
conarti <abelous2009@gmail.com>
2023-05-29 22:25:58 +0300

    docs(config): add npm link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -38,6 +38,12 @@ export default defineConfig({
 				icon: 'github',
 				link: 'https://github.com/conarti/eslint-plugin-fsd',
 			},
+			{
+				icon: {
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" aria-label="npm" role="img" viewBox="0 0 512 512"><rectwidth="512" height="512"rx="15%"fill="#fff"/><path fill="none" stroke="#cb3837" stroke-width="22" d="m234 311h213v-112H65v112h113V210m-12 123h90M133 232v68m112-68v45m45-67v90m67-68v68m45-68v68"/></svg>'
+				},
+				link: 'https://www.npmjs.com/package/eslint-plugin-conarti-fsd',
+			},
 		],
 
 		search: {
