7a3bcf5c6bd5f96a93410780c9a02bedebb4a9de
conarti <abelous2009@gmail.com>
2023-06-09 11:54:25 +0300

    docs: setup sidebar links and update description with title

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-	title: 'Feature Sliced Eslint Plugin',
-	description: 'Feature Sliced Design Eslint Plugin Docs',
+	title: 'eslint-plugin-conarti-fsd',
+	description: 'feature-sliced.design Eslint Plugin Docs',
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [
@@ -19,15 +19,32 @@ export default defineConfig({
 
 		sidebar: [
 			{
-				text: 'Examples',
+				text: 'Getting Started',
 				items: [
 					{
-						text: 'Markdown Examples',
-						link: '/markdown-examples',
+						text: 'Quick install',
+						link: '/quick-install',
 					},
 					{
-						text: 'Runtime API Examples',
-						link: '/api-examples',
+						text: 'Advanced configuration',
+						link: '/advanced-configuration',
+					},
+				],
+			},
+			{
+				text: 'Rules',
+				items: [
+					{
+						text: 'absolute-relative',
+						link: '/rules/absolute-relative',
+					},
+					{
+						text: 'layers-slices',
+						link: '/rules/layers-slices',
+					},
+					{
+						text: 'public-api',
+						link: '/rules/public-api',
 					},
 				],
 			},
@@ -40,7 +57,7 @@ export default defineConfig({
 			},
 			{
 				icon: {
-					svg: '<svg xmlns="http://www.w3.org/2000/svg" aria-label="npm" role="img" viewBox="0 0 512 512"><rectwidth="512" height="512"rx="15%"fill="#fff"/><path fill="none" stroke="#cb3837" stroke-width="22" d="m234 311h213v-112H65v112h113V210m-12 123h90M133 232v68m112-68v45m45-67v90m67-68v68m45-68v68"/></svg>'
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" aria-label="npm" role="img" viewBox="0 0 512 512"><rectwidth="512" height="512"rx="15%"fill="#fff"/><path fill="none" stroke="#cb3837" stroke-width="22" d="m234 311h213v-112H65v112h113V210m-12 123h90M133 232v68m112-68v45m45-67v90m67-68v68m45-68v68"/></svg>',
 				},
 				link: 'https://www.npmjs.com/package/eslint-plugin-conarti-fsd',
 			},
