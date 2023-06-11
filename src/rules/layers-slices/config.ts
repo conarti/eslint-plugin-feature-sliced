1f61dd387cbd925a852f45e763e0457c3121beea
conarti <abelous2009@gmail.com>
2023-06-11 12:25:39 +0300

    refactor(layers-slices): simplify MessageIds type

--- src/rules/layers-slices/config.ts
+++ src/rules/layers-slices/config.ts
@@ -2,7 +2,7 @@ export const enum ERROR_MESSAGE_ID {
   CAN_NOT_IMPORT = 'can-not-import',
 }
 
-export type MessageIds = typeof ERROR_MESSAGE_ID[keyof typeof ERROR_MESSAGE_ID];
+export type MessageIds = ERROR_MESSAGE_ID;
 
 export type Options = [
   {
