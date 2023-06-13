3bb1ed4a3d747794516a1c619f067dd97d03cff2
conarti <abelous2009@gmail.com>
2023-06-13 21:57:54 +0300

    chore(convert-to-absolute): add todo

--- src/lib/path-lib/convert-to-absolute.ts
+++ src/lib/path-lib/convert-to-absolute.ts
@@ -1,6 +1,7 @@
 import { joinPath } from './join-path';
 import { isPathRelative } from './is-path-relative';
 
+/* TODO: rename 'fromPath' to 'base', 'targetPath' to 'target' */
 export function convertToAbsolute(fromPath: string, targetPath: string): string {
   if (targetPath === '') {
     return fromPath;
