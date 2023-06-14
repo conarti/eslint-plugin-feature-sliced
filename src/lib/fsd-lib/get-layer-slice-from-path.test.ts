f1b08f406b24227382c3d857df35d115ff73b3da
conarti <abelous2009@gmail.com>
2023-06-14 14:55:27 +0300

    test(get-layer-slice-from-path): use 'it.each'

--- src/lib/fsd-lib/get-layer-slice-from-path.test.ts
+++ src/lib/fsd-lib/get-layer-slice-from-path.test.ts
@@ -1,125 +1,85 @@
 import { getLayerSliceFromPath } from './get-layer-slice-from-path';
 
-/* TODO: refactor using 'it.each' */
+const layerCases = [
+  {
+    path: 'src/entities/foo',
+    expected: 'entities',
+  },
+  {
+    path: 'some/long/root/path/entities/foo',
+    expected: 'entities',
+  },
+  {
+    path: 'entities/foo',
+    expected: 'entities',
+  },
+  {
+    path: 'EntitiEs/foo',
+    expected: 'entities',
+  },
+  {
+    path: './index.ts',
+    expected: null,
+  },
+  {
+    path: './app.ts',
+    expected: null,
+  },
+  {
+    path: './entitiesInName.ts',
+    expected: null,
+  },
+];
+
+const sliceCases = [
+  {
+    path: 'src/entities/foo',
+    expected: 'foo',
+  },
+  {
+    path: 'entities/foo',
+    expected: 'foo',
+  },
+  {
+    path: 'entities/foo/bar/baz',
+    expected: 'foo',
+  },
+  {
+    path: 'some/long/path/entities/foo/bar/baz',
+    expected: 'foo',
+  },
+  {
+    path: 'some/long/path/entities/foo/bar/baz.tsx',
+    expected: 'foo',
+  },
+  {
+    path: 'src/app/App.tsx',
+    expected: null,
+  },
+  {
+    path: 'src/app',
+    expected: null,
+  },
+  {
+    path: '/Users/conarti/Projects/feature-sliced-frontend/src/entities/foo-bar-baz/ui/index.vue',
+    expected: 'foo-bar-baz',
+  },
+];
 
 describe('get-layer-slice-from-path', () => {
-  it('should return layer 1', () => {
-    const path = 'src/entities/foo';
-    const expectedLayer = 'entities';
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
+  it.each(layerCases)('should correct return layer %#', ({
+    path,
+    expected,
+  }) => {
+    const [actual] = getLayerSliceFromPath(path);
+    expect(actual).toBe(expected);
   });
 
-  it('should return layer 2', () => {
-    const path = 'some/long/root/path/entities/foo';
-    const expectedLayer = 'entities';
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
-  });
-
-  it('should return layer 3', () => {
-    const path = 'entities/foo';
-    const expectedLayer = 'entities';
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
-  });
-
-  it('should return layer 4', () => {
-    const path = 'EntitiEs/foo';
-    const expectedLayer = 'entities';
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
-  });
-
-  it('should not return layer 1', () => {
-    const path = './index.ts';
-    const expectedLayer = null;
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
-  });
-
-  it('should not return layer 2', () => {
-    const path = './app.ts';
-    const expectedLayer = null;
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
-  });
-
-  it('should not return layer 3', () => {
-    const path = './entitiesInName.ts';
-    const expectedLayer = null;
-    const [actualLayer] = getLayerSliceFromPath(path);
-
-    expect(actualLayer).toStrictEqual(expectedLayer);
-  });
-
-  it('should return slice 1', () => {
-    const path = 'src/entities/foo';
-    const expectedSlice = 'foo';
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should return slice 2', () => {
-    const path = 'entities/foo';
-    const expectedSlice = 'foo';
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should return slice 3', () => {
-    const path = 'entities/foo/bar/baz';
-    const expectedSlice = 'foo';
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should return slice 4', () => {
-    const path = 'some/long/path/entities/foo/bar/baz';
-    const expectedSlice = 'foo';
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should return slice 5', () => {
-    const path = 'some/long/path/entities/foo/bar/baz.tsx';
-    const expectedSlice = 'foo';
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should not return slice 6', () => {
-    const path = 'src/app/App.tsx';
-    const expectedSlice = null;
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should not return slice 7', () => {
-    const path = 'src/app';
-    const expectedSlice = null;
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
-  });
-
-  it('should not return slice 8 (kebab-case)', () => {
-    const path = '/Users/conarti/Projects/feature-sliced-frontend/src/entities/foo-bar-baz/ui/index.vue';
-    const expectedSlice = 'foo-bar-baz';
-    const [, actualSlice] = getLayerSliceFromPath(path);
-
-    expect(actualSlice).toStrictEqual(expectedSlice);
+  it.each(sliceCases)('should correct return slice %#', ({
+    path,
+    expected,
+  }) => {
+    const [, actual] = getLayerSliceFromPath(path);
+    expect(actual).toBe(expected);
   });
 });
