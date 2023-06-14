3e437ddc4c556a609aef55ddbcd8991b1207b9ef
conarti <abelous2009@gmail.com>
2023-06-14 10:26:23 +0300

    fix(rule-lib): add valid ImportExpression model

--- src/lib/rule-lib/models.ts
+++ src/lib/rule-lib/models.ts
@@ -1,6 +1,12 @@
 import type { TSESTree } from '@typescript-eslint/utils';
 
+/**
+ *  TSESTree has invalid type for this node.
+ *  https://astexplorer.net/#/gist/c72aaf4c7f7b4d57f554a4354f604d36/c416e580065f25d111fef37323e6001e2ea67ec1
+ */
+export type ImportExpression = TSESTree.ImportExpression & { source: TSESTree.StringLiteral }
+
 export type ImportExportNodes = TSESTree.ImportDeclaration
-	| TSESTree.ImportExpression
+	| ImportExpression
 	| TSESTree.ExportAllDeclaration
 	| TSESTree.ExportNamedDeclaration;
