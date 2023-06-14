ad8eca844869848216f67d59343e2738444adf70
conarti <abelous2009@gmail.com>
2023-06-14 12:18:48 +0300

    refactor(is-ignored): replace micromatch to picomatch

--- src/lib/rule-lib/is-ignored.ts
+++ src/lib/rule-lib/is-ignored.ts
@@ -1,8 +1,14 @@
-import micromatch from 'micromatch';
+import picomatch from 'picomatch';
 
 /**
  * Checks if the path needs to be validated against patterns to ignore
  */
-export function isIgnored(path: string, patterns: string[] = null): boolean {
-  return patterns && micromatch.isMatch(path, patterns);
+export function isIgnored(path: string, patterns?: string[]): boolean {
+  if (!Array.isArray(patterns)) {
+    return false;
+  }
+
+  const match = picomatch(patterns);
+
+  return match(path);
 }
