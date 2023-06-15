d795dc1aa99f8090f42b3ff03c7edc6416d608e5
conarti <abelous2009@gmail.com>
2023-06-15 12:24:25 +0300

    test(vitest): setup coverage + output in ui

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      include: ['**/src/**'],
+      exclude: ['**/rule-lib/models.ts'],
+      all: true,
+      clean: true,
+      cleanOnRerun: true,
+      reporter: ['html'],
+    },
   },
 });
