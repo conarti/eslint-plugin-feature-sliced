c6129a9df542eda0b194b3dbde8288b6d050b3da
conarti <abelous2009@gmail.com>
2023-06-16 10:59:02 +0300

    refactor(extract-paths-info): add 'hasNotSlice' variable

--- src/lib/fsd-lib/extract-paths-info.ts
+++ src/lib/fsd-lib/extract-paths-info.ts
@@ -29,6 +29,7 @@ export function extractPathsInfo(node: ImportExportNodesWithSourceValue, context
   const isSameSlice = importSlice === currentFileSlice;
   const isInsideShared = currentFileLayer === 'shared' && importLayer === 'shared';
   const isInsideApp = currentFileLayer === 'app' && importLayer === 'app';
+  const hasNotSlice = !importSlice;
   const hasNotCurrentFileSlice = !currentFileSlice;
 
   /** TODO: move getting 'segment', 'segmentFiles' logic to this func. Delete 'getFsdPartsFromPath'  */
@@ -58,6 +59,7 @@ export function extractPathsInfo(node: ImportExportNodesWithSourceValue, context
     isSameSegment,
     isInsideShared,
     isInsideApp,
+    hasNotSlice,
     hasNotCurrentFileSlice,
   };
 }
