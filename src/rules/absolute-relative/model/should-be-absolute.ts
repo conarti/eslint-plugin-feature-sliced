1174b6672c6d78d33a976266cad683ce4440563a
conarti <abelous2009@gmail.com>
2023-06-16 13:03:13 +0300

    refactor(absolute-relative): use 'hasUnknownLayers' from PathsInfo

--- src/rules/absolute-relative/model/should-be-absolute.ts
+++ src/rules/absolute-relative/model/should-be-absolute.ts
@@ -1,24 +1,18 @@
 import { type PathsInfo } from '../../../lib/fsd-lib';
 
-/* TODO refactor this */
 export function shouldBeAbsolute(pathsInfo: PathsInfo): boolean {
   const {
     isRelative,
     importLayer,
     currentFileLayer,
-    hasNotLayer,
-    hasNotCurrentFileLayer,
+    hasUnknownLayers,
   } = pathsInfo;
 
   if (!isRelative) {
     return false;
   }
 
-  if (hasNotLayer) {
-    return false;
-  }
-
-  if (hasNotCurrentFileLayer) {
+  if (hasUnknownLayers) {
     return false;
   }
 
