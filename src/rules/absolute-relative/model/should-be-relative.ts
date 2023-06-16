188a1640311b0e88733fc4426b2055a52a49e088
conarti <abelous2009@gmail.com>
2023-06-16 13:12:55 +0300

    refactor(absolute-relative)!: remove validation 'hasNotLayer || hasNotSlice' from should be relative check
    BREAKING-CHANGE: The check has been removed because is not covered by tests. If there are any problems with this rule - revert this commit and update tests

--- src/rules/absolute-relative/model/should-be-relative.ts
+++ src/rules/absolute-relative/model/should-be-relative.ts
@@ -1,24 +1,15 @@
-import {
-  type PathsInfo,
-} from '../../../lib/fsd-lib';
+import { type PathsInfo } from '../../../lib/fsd-lib';
 
-/* TODO refactor this */
 export function shouldBeRelative(pathsInfo: PathsInfo): boolean {
   const {
     isRelative,
     isSameLayerWithoutSlices,
     isSameLayer,
     isSameSlice,
-    hasNotLayer,
-    hasNotSlice,
     hasNotCurrentFileSlice,
   } = pathsInfo;
 
   if (isRelative) {
-    return false;
-  }
-
-  if (hasNotLayer || hasNotSlice) {
     return false;
   }
 
