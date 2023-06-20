ace89b291fa5ead680bbc742a0f738813e3cd761
conarti <abelous2009@gmail.com>
2023-06-20 23:30:59 +0300

    refactor(extract-layer): simplify logic

--- src/lib/fsd-lib/extract-layer.ts
+++ src/lib/fsd-lib/extract-layer.ts
@@ -3,25 +3,14 @@ import {
   layers,
 } from '../../config';
 import { getByRegExp } from '../shared';
-import { isLayer } from './layers';
 
 /**
  * Returns the layer from the path
  */
 export function extractLayer(targetPath: string): Layer | null {
   const layersRegExpPattern = `(${layers.join('|')})(?![\\w\\.-])`;
+  const layersRegExp = new RegExp(layersRegExpPattern, 'ig');
+  const lowerCasedTargetPath = targetPath.toLowerCase();
 
-  const layer = getByRegExp<Layer>(targetPath, new RegExp(layersRegExpPattern, 'ig'), true);
-
-  if (typeof layer !== 'string') {
-    return null;
-  }
-
-  const lowercasedLayer = layer.toLowerCase();
-
-  if (!isLayer(lowercasedLayer)) {
-    return null;
-  }
-
-  return lowercasedLayer;
+  return getByRegExp<Layer>(lowerCasedTargetPath, layersRegExp, true);
 }
