5950283e578e601e1cc3d1df024b1b6491813c79
conarti <abelous2009@gmail.com>
2023-06-25 18:16:04 +0300

    refactor(is-ignored-current-file): fix cycle imports

--- src/lib/rule-lib/is-ignored-current-file.ts
+++ src/lib/rule-lib/is-ignored-current-file.ts
@@ -1,9 +1,7 @@
-import {
-  extractCurrentFilePath,
-  extractRuleOptions,
-  isIgnored,
-  type UnknownRuleContext,
-} from './index';
+import { extractCurrentFilePath } from './extract-current-file-path';
+import { extractRuleOptions } from './extract-rule-options';
+import { isIgnored } from './is-ignored';
+import { type UnknownRuleContext } from './models';
 
 export function isIgnoredCurrentFile<
   OptionsWithPatterns extends Readonly<[{ ignoreInFilesPatterns?: string[] }]>
