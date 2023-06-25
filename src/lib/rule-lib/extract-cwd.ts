9ab3c7494774d37656779f33725165e3899ff780
conarti <abelous2009@gmail.com>
2023-06-25 17:56:08 +0300

    refactor(extract-cwd): add return types

--- src/lib/rule-lib/extract-cwd.ts
+++ src/lib/rule-lib/extract-cwd.ts
@@ -2,7 +2,7 @@ import { normalizePath } from '../path-lib';
 import { isUndefined } from '../shared';
 import { type UnknownRuleContext } from './models';
 
-export function extractCwd(context: UnknownRuleContext) {
+export function extractCwd(context: UnknownRuleContext): string | undefined {
   const cwd = context.getCwd?.();
 
   if (isUndefined(cwd)) {
