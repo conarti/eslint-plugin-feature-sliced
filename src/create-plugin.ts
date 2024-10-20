b15162367083223508164d910a4ca83da1fb7f1b
conarti <abelous2009@gmail.com>
2024-10-20 03:13:12 +0300

    feat: add settings for each rule

--- src/create-plugin.ts
+++ src/create-plugin.ts
@@ -1,20 +1,61 @@
 import type { ImportOrderConfigName, TypedFlatConfigItem } from './config';
+import type { VALIDATION_LEVEL } from './rules/public-api/config';
 import { mergeConfigs } from 'eslint-flat-config-utils';
 import { PLUGIN_NAME } from './config';
 import { importOrder } from './configs/import-order';
 import { plugin } from './plugin';
 
-interface ESLintPluginFeatureSlicedOptions {
+interface AbsoluteRelativeOptions {
   /**
-   * Import sorting type. Use 'false' to disable.
-   * @default 'recommended'
+   * Disable the rule in certain files
    */
+  ignoreInFilesPatterns: string[];
+}
+
+interface LayersSlicesOptions {
+  /**
+   * Ignore cross-imports of types
+   * @default true
+   */
+  allowTypeImports: boolean;
+  /**
+   * Ignore certain import paths (import foo from '<path-to-ignore>')
+   */
+  ignorePatterns: string[];
+  /**
+   * Disable the rule in certain files
+   */
+  ignoreInFilesPatterns: string[];
+}
+
+interface PublicApiOptions {
+  /**
+   * Adjusts the depth.
+   * 'slices' will check for presence 'index' file at the slice level only,
+   * 'segments' at the slice and its segments level
+   * Default is 'slices', but 'segments' is recommended
+   * @default 'slices'
+   */
+  level: VALIDATION_LEVEL;
+  /**
+   * Disable the rule in certain files
+   */
+  ignoreInFilesPatterns: string[];
+}
+
+interface ESLintPluginFeatureSlicedOptions {
   sortImports?: false | ImportOrderConfigName;
+  absoluteRelative?: AbsoluteRelativeOptions;
+  layersSlices?: LayersSlicesOptions;
+  publicApi?: PublicApiOptions;
 }
 
 export function createPlugin(options: ESLintPluginFeatureSlicedOptions = {}): TypedFlatConfigItem {
   const {
     sortImports = 'recommended',
+    absoluteRelative = {},
+    layersSlices = {},
+    publicApi = {},
   } = options;
 
   const createRuleName = (rule: string): string => `${PLUGIN_NAME}/${rule}`;
@@ -25,9 +66,9 @@ export function createPlugin(options: ESLintPluginFeatureSlicedOptions = {}): Ty
       [PLUGIN_NAME]: plugin,
     },
     rules: {
-      [createRuleName('layers-slices')]: 'error',
-      [createRuleName('absolute-relative')]: 'error',
-      [createRuleName('public-api')]: 'error',
+      [createRuleName('layers-slices')]: ['error', layersSlices],
+      [createRuleName('absolute-relative')]: ['error', absoluteRelative],
+      [createRuleName('public-api')]: ['error', publicApi],
     },
   } satisfies TypedFlatConfigItem;
 
