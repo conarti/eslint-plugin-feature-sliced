b3425654077c915b37f0bd52b228c77f1bc85b9d
conarti <abelous2009@gmail.com>
2024-12-07 23:27:43 +0300

    refactor(configs): remove unused config files in configs

--- src/configs/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ESLint } from 'eslint';
-import { importOrder } from './import-order';
-import recommended from './recommended';
-
-export const configs = {
-  recommended,
-  importOrder,
-} as ESLint.Plugin['configs'];
