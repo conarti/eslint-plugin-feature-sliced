462d17045dd4153cba95a05acebd3786cc589fbb
Aleksandr Belous <a.belous@localtrip.ru>
2026-01-13 21:24:04 +0300

    feat: add named exports for tseslint.config() compatibility
    Export plugin object and config constants for manual configuration.
    Users can now use the plugin in two ways:
    
    1. Via createPlugin function (recommended):
       featureSliced({ /* options */ })
    
    2. Via plugin object (manual):
       { plugins: { '@conarti/feature-sliced': plugin } }
    
    Exports: createPlugin, plugin, PLUGIN_NAME, RULE_NAMES, layers, segments
    Types: TypedFlatConfigItem, Layer, Segment, ImportOrderConfigName

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { createPlugin } from './create-plugin';
+import { plugin } from './plugin';
+
+export { createPlugin };
+export { plugin };
+export { layers, PLUGIN_NAME, RULE_NAMES, segments } from './config';
+export type { ImportOrderConfigName, Layer, Segment, TypedFlatConfigItem } from './config';
 
 export default createPlugin;
