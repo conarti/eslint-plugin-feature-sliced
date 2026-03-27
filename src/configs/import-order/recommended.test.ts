ad63b2a1e471ca4f5c0937c165fd634fa86bbacb
conarti <abelous2009@gmail.com>
2026-03-27 11:06:41 +0300

    fix: убрать лишний комментарий - overrideConfigFile: true валидно в ESLint 10

--- src/configs/import-order/recommended.test.ts
+++ src/configs/import-order/recommended.test.ts
@@ -2,11 +2,6 @@ import { ESLint } from 'eslint';
 import { configLib } from '../../../tests/utils';
 import { recommended } from './recommended';
 
-/*
- * overrideConfigFile: true отключает поиск config-файла в ESLint 9.
- * При переходе на ESLint 10 нужно убрать эту опцию (она удалена),
- * flat config используется по умолчанию.
- */
 const eslint = new ESLint({
   overrideConfigFile: true,
   overrideConfig: [configLib.setParser(recommended)],
